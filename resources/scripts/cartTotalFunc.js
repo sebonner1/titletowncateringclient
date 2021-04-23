a4daf727c3112224819946de27b6fa3100a2e5f9
Sydney Bonner <sebonner1@crimson.ua.edu>
2021-04-23 16:49:45 -0500

    list works

--- resources/scripts/cartTotalFunc.js
+++ resources/scripts/cartTotalFunc.js
@@ -10,7 +10,7 @@ function postCart()
             "Accept": 'application/json',
             "Content-Type": 'application/json',
         },
-        body: JSON.stringify()
+        body: JSON.stringify() // use this here
         // console.log("made oit past the fetch"),
     }).then((response)=>{
         console.log(response);
---
+++ resources/scripts/getList.js
@@ -0,0 +1,93 @@
+var cart = [];
+var items = [
+    {
+        'id': 1,
+        'description': 'Chicken Parm',
+        'price': 11.50,
+        'count': 1
+    },
+    {
+        'id':2,
+        'description': 'Chocolate Chip Cookie(s)',
+        'price': 2.50,
+        'count': 1
+    },
+    {
+        'id': 3,
+        'description': 'Veggie Burger',
+        'price': 10.00,
+        'count': 1
+    },
+    {
+        'id':4,
+        'description': 'Chicken Salad',
+        'price': 6.75,
+        'count': 1
+    },
+    {
+        'id': 5,
+        'description': 'Turkey Melt',
+        'price': 5.25,
+        'count': 1
+    },
+    {
+        'id': 6,
+        'description': 'House Salad',
+        'price': 7.50,
+        'count': 1
+    }
+]
+
+function handleOnLoad(){
+    populateCart();
+}
+
+function handleAddItem(id){
+    var found = false;
+    cart.forEach((item)=>{
+        if(item.id==id){
+            handleAddOne(id);
+            found=true;
+        }
+    })
+    if(!found){
+        items.forEach((item)=>{
+            if(item.id==id){
+                cart.push(item);
+            }
+        })
+    }
+    populateCart();
+}
+
+function handleAddOne(id){
+    cart.forEach((item)=>{
+        if(item.id==id){
+            item.count++;
+        }
+    })
+    populateCart();
+}
+
+function handleSubOne(id){
+    cart.forEach((item)=>{
+        if(item.id==id){
+            item.count--;
+        }
+    });
+    populateCart();
+}
+
+function populateCart(){
+    let html= "<table>";
+    html += "<tr><th>Description</th><th style: \"text-align:center;\">Price</th><th>Count</th><th>Total</th></tr>";
+    cart.forEach((item) =>{
+      html+="<tbody><td>"+item.description+"</td>";
+      html+="<td>$"+item.price+"</td>";
+      html+="<td>"+item.count +"</td>";
+      html+="<td>"+(item.count * item.price) +"</td>";
+      html+="<td><button onclick=\"handleAddOne("+item.id+")\">+</button></td>";
+      html+="<td><button onclick=\"handleSubOne("+item.id+")\">-</button></td></tbody>";
+    });
+    document.getElementById("cart").innerHTML= html;
+}
