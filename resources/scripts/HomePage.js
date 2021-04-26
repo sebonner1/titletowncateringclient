354b10611607b82272758bf2237324c8b0a5749e
Sydney Bonner <sebonner1@crimson.ua.edu>
2021-04-26 03:16:51 -0500

    trying index here

--- resources/scripts/HomePage.js
+++ resources/scripts/HomePage.js
@@ -3,7 +3,7 @@ const startorder = document.getElementById("startorder");
 // the below syntax from research and is an API
 startorder.addEventListener("click", (e) => {
         alert("You have begun your order.");
-        window.location.href= "../resources/HomePage.html";
+        window.location.href= "../resources/index.html";
 })
 function pushCartData(){
         const postCartAPIURL = "https://ttcapi.herokuapp.com/api/reports"; // "https://localhost:5000/api/reports"; // https://ttcapi.herokuapp.com/
