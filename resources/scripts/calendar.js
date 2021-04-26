90599f9cb40e7151107b0bd6a90714a8cbe9a39b
Sydney Bonner <sebonner1@crimson.ua.edu>
2021-04-26 11:57:45 -0500

    working on the calendar

--- resources/scripts/calendar.js
+++ resources/scripts/calendar.js
@@ -25,29 +25,29 @@ loadTimes();
  
  
 
-function loadTimes() 
-{ 
+// function loadTimes() 
+// { 
 
-var timeSel = document.getElementById("time"); 
-timeSel.innerHTML = ` 
-<option value="" selected="selected">Please select a day first</option> 
-<option value="7:00 AM" id= "timeselection">7:00 AM</option> 
-<option value="8:00 AM" id= "timeselection">8:00 AM</option> 
-<option value="9:00 AM" id= "timeselection">9:00 AM</option> 
-<option value="10:00 AM" id= "timeselection">10:00 AM</option> 
-<option value="11:00 AM" id= "timeselection">11:00 AM</option> 
-<option value="12:00 PM" id= "timeselection">12:00 PM</option> 
-<option value="1:00 PM" id= "timeselection">1:00 PM</option> 
-<option value="2:00 PM" id= "timeselection">2:00 PM</option> 
-<option value="3:00 PM" id= "timeselection">3:00 PM</option> 
-<option value="4:00 PM" id= "timeselection">4:00 PM</option> 
-<option value="5:00 PM" id= "timeselection">5:00 PM</option> 
-<option value="6:00 PM" id= "timeselection">6:00 PM</option> 
-<option value="7:00 PM" id= "timeselection">7:00 PM</option> 
-`;
-} 
+// var timeSel = document.getElementById("time"); 
+// // timeSel.innerHTML = ` 
+// // <option value="" selected="selected">Please select a day first</option> 
+// // <option value="7:00 AM" id= "timeselection">7:00 AM</option> 
+// // <option value="8:00 AM" id= "timeselection">8:00 AM</option> 
+// // <option value="9:00 AM" id= "timeselection">9:00 AM</option> 
+// // <option value="10:00 AM" id= "timeselection">10:00 AM</option> 
+// // <option value="11:00 AM" id= "timeselection">11:00 AM</option> 
+// // <option value="12:00 PM" id= "timeselection">12:00 PM</option> 
+// // <option value="1:00 PM" id= "timeselection">1:00 PM</option> 
+// // <option value="2:00 PM" id= "timeselection">2:00 PM</option> 
+// // <option value="3:00 PM" id= "timeselection">3:00 PM</option> 
+// // <option value="4:00 PM" id= "timeselection">4:00 PM</option> 
+// // <option value="5:00 PM" id= "timeselection">5:00 PM</option> 
+// // <option value="6:00 PM" id= "timeselection">6:00 PM</option> 
+// // <option value="7:00 PM" id= "timeselection">7:00 PM</option> 
+// // `;
+// } 
 
-const timeSelection = document.getElementById("timeselection"); 
+const timeSelection = document.getElementById("time"); 
 // const daySelection = document.getElementById("weekdays_short"); 
 var addressInput = document.getElementById("address"); // question in office hours: if the value changes but the id is the same, what do I call here? 
 console.log(timeSelection); 
