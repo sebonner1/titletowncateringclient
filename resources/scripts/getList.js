bc00d5ff3837deacbf3ca39e2d829870a021acf0
Sydney Bonner <sebonner1@crimson.ua.edu>
2021-04-28 17:01:36 -0500

    fixing getlist

--- resources/scripts/getList.js
+++ resources/scripts/getList.js
@@ -138,6 +138,7 @@ function pushCartItems()
         body: JSON.stringify(finalCartItems)
     }).then((response)=>{
         console.log(response);
+        window.location.href = "Calendar.html";
     })
 }
 
