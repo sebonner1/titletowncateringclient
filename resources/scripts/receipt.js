e4fa63f3141cf8034853a3bac83a9520fa68c1aa
Katherine Pearson <katherine@KatherinesMBP2.fios-router.home>
2021-04-29 14:22:39 -0500

    I think this works

--- resources/scripts/receipt.js
+++ resources/scripts/receipt.js
@@ -107,7 +107,7 @@ function getCartItemsReceipt(){
             html += 'Tax: $' +tax+ "</td></tr></tbody>",
             html += '<tbody><tr><td style="text-align:right; font-weight: bold;">',
             html += 'Suggested Tip (15%): $' +suggestedTip+ "",
-            html += '<form> <label for = "Tip">Please enter your Tip</label>input type = "text" id = "tip"</form>',
+            html += '<form> <label for = "Tip">Please enter your Tip</label><input type = "text" id = "tip"></form>',
             html += totalPrice = (totalPricemid + tip)
             html += 'Total Price: $'+totalPrice+"</td></tr></tbody>",
             html += "</ul>";
