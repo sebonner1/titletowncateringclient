eca47678ede8a8f817992c2af655a87745c4eb85
Katherine Pearson <katherine@KatherinesMBP2.fios-router.home>
2021-04-29 15:10:21 -0500

    here

--- resources/scripts/receipt.js
+++ resources/scripts/receipt.js
@@ -97,8 +97,8 @@ function getCartItemsReceipt(){
                 html += "<td>"+item.quantity + " " + item.itemName+"</td>",
                 html += '<td class="alignright">'+'$'+(item.quantity * item.price)+"</td>",
                 priceBeforeTax += (item.quantity * item.price),
-                tax += round((priceBeforeTax*.1),2),
-                suggestedTip += round(((priceBeforeTax + tax) * .15),2),
+                tax += math.round((priceBeforeTax*.1),2),
+                suggestedTip += math.round(((priceBeforeTax + tax) * .15),2),
                 totalPricemid+= (tax+priceBeforeTax),
                 html += "</tr>",
                 html += "</tbody></table>";
